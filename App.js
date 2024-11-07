f26cde1ddb2558b28ef1ad3bbbf573496c7d7a21
GabrielCarvalhoRodrigues <145344488+GabrielCarvalhoRodrigues@users.noreply.github.com>
2024-11-06 22:43:51 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import InventoryView from './src/views/InventoryView';
 import Login from './src/views/login';
-import ManageInventory from './src/views/ManageInventory';
+import MenageInventory from './src/views/MenageInventory';
 
 const Stack = createNativeStackNavigator();
 
